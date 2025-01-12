0eebd94f2dbc541857807975b5a8ab2566d00cd7
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2025-01-12 05:19:44 +0000

    minor chage

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,8 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'adityadeshmukh7350@gmail.com',
+    phone: '+918605928558',
+    address: 'Satara, Maharashtra - 415512',
+    github: 'https://github.com/Aditya664',
+    linkedIn: 'https://www.linkedin.com/in/adityadeshmukh664/',
+    stackOverflow: 'https://stackoverflow.com/users/29160721/aditya-deshmukh',
 }
